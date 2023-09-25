e34e2eaa7d9b4b2396baf3fdeec3c6a995eb59a1
jibundeyare <contact@jibundeyare.com>
2023-09-25 16:55:09 +0200

    Création d'un moteur de rendu

--- app.js
+++ app.js
@@ -2,22 +2,29 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  let html = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World!</title>
-</head>
-<body>
-    <h1>Hello World!</h1>
-</body>
-</html>
-`
+function render(data) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>${data.title}</title>
+  </head>
+  <body>
+    ${data.body}
+  </body>
+  </html>`
+}
 
-  res.send(html)
+app.get('/', (req, res) => {
+  let title = 'Hello World!'
+  let body = '<h1>Hello World!</h1>'
+
+  res.send(render({
+    title: title,
+    body: body
+  }))
 })
 
 app.listen(port, () => {
