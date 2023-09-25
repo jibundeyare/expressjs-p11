780a6802d2edb31a20f0124d16d42f9862b235de
jibundeyare <contact@jibundeyare.com>
2023-09-25 16:29:56 +0200

    Amélioration de la page d'accueil

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  let html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World!</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
+`
+
+  res.send(html)
 })
 
 app.listen(port, () => {
